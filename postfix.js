6894f80cd925a1356da70b8b5e9d7b25308a2580
Craig <cav9871@saintjoe.edu>
2015-02-04 01:02:25 -0500

    I tried many different ways and this my current idea, it does not add the operators to the output string. That is my only problem right now, and I don't have it calcualting yet.

--- postfix.js
+++ postfix.js
@@ -4,56 +4,71 @@
 
 
 function Stack() {
-	this.operators = [];
-	this.operands = [];
-	this.top = 0;
+	this.dataStore = [];
+	this.serparte = separate;
 	this.push = push;
 	this.pop = pop;
-}
-
-function Output() {
-	this.operands = [];
 	this.top = 0;
-	this.push = push;
-	this.pop = pop;
 }
 
-
-function push(userInput) {
-	//var out = new Output();
-	print('I am getting ' + userInput);
-	for (var i = 0; i < userInput.length;i++) {
-		var x = userInput.charAt(i);
-		if( isNaN(x)) {
-		   if ((x == '*') ||(x == '-') || (x == '+') || (x == '/')) {
-			print(' this is a stack ' + x);
-				this.operators[this.top++] = x;
-				print('top index for operators is ' + this.top);
-				
-		   }
-		
-		}
-		         else {
-	                      print('this is the output ' + x);
-			      this.operands[this.top++] = x;
-			      print('top index for operand is ' + this.top);
-		         }   	     
-
-	//return this.dataStore;
-	}
+function push(element) {
+	this.dataStore[this.top++] = element;
 }
-
-
-
 function pop() {
 	return this.dataStore[--this.top];
 }
 
-var s = new Stack();
-print('Enter infix expression : ');
-var expression = readline('Enter infix expression');
-s.push(expression);
-print(s.dataStore);
-//print(s.dataStore);
-//print(s.push(userInput));
-//print(s);
+
+
+function separate(userInput) {                                          // separates the operators from operands
+	var s = new Stack();                         // creates new statck
+	
+	var str = '';                                // creates new empty sting to store output
+	for (var i = 0; i < userInput.length;i++) {  // searches through each character 
+		var x = userInput.charAt(i);         // stores each character as x
+		
+		if(!isNaN(x)) {                      // sees whether x is a number
+			//print(str);
+			//s.push(x);
+		        str += x;                    // puts x in output string 
+		}
+		   
+		else if((x == '+') || (x == '-') || (x == '*') 
+				|| (x == '/') || (x == '^')) {              // checks to see which operator it is
+			   //print('about to push operator ' + x);          
+			   //print('operators added ' + str);
+                           s.push(x);                                       // pushes x to the stack
+			   //print(s.dataStore);
+			   
+			  
+			   while((x != '^') && (precedence(x) > 1)) {       // checks  to see if the precedence is greater than one 
+				 str += x;
+			         s.pop();	 
+			  }
+			   }
+
+		   }
+	print(str);
+        }
+
+	
+var precedence = function(operator) {                                // gives each operator a rank
+
+       switch(operator) {
+	       case '+':
+	       case '-':	       
+		return 1;
+	       case '*':
+	       case '/':
+	        return 2;
+	       case '^':
+	        return 3;
+	       default:
+	        return 0;	
+       }
+}       
+
+
+//print('Enter infix expression : ');
+//var expression = readline('Enter infix expression');
+var test = new separate('2+3+4+5+6');
